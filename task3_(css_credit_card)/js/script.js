4c5abcc5cf934439f3dc70509c826d18561d23b4
Ihor Yokhym <igoryoxum@gmail.com>
2018-11-08 16:12:58 +0200

    Update script.js
    - fix bug: card rotate

--- task3_(css_credit_card)/js/script.js
+++ task3_(css_credit_card)/js/script.js
@@ -60,24 +60,28 @@ window.onload = function(){
     }
   });
   //CVV
-  $('#cardCVVInput').focus(function() {
-  // 	var dimensions = document.getElementById("Rotate").getBoundingClientRect();
+  $('#cardCVVInput').focus(function() { 	
+  	var rotate = document.getElementById('Rotate');
+  	rotate90();
+  	setTimeout(rotate180,750);
+  	function rotate90(){
+	  	rotate.style.transform = "rotateY(90deg)";
+	  	rotate.style.transition  = "all 0.75s";
+    }
+    function rotate180(){
+		  document.getElementById('card-number').style.display = 'none';
+	    document.getElementById('card-expire').style.display = 'none';
+	    document.getElementById('card-holder').style.display = 'none';
+	    document.getElementById('imgCard').style.display = 'none';
+	    document.getElementById('cvv').style.display = 'flex';
+	    document.getElementById('cvv').style.transform = 'scaleX(-1)';
+	    document.getElementById('card-company').style.transform = 'scaleX(-1)';
+		  rotate.style.transform = "rotateY(180deg)";
+	  	rotate.style.transition  = "all 0.75s";
+  }
 
-		// document.body.appendChild(document.createTextNode(dimensions.width + " x " + dimensions.height));
-  	document.getElementById('Rotate').style.transform = "rotateY(180deg)";
-  	// var b = 0;
-  	// var a = document.getElementById('Rotate').style.transform = "rotateY(" + b + "deg)";
-  	document.getElementById('Rotate').style.transition  = "all 1.5s";
-  	// if (document.getElementById('Rotate').style.transform == "rotateY(180deg)"){
-    document.getElementById('card-number').style.display = 'none';
-    document.getElementById('card-expire').style.display = 'none';
-    document.getElementById('card-holder').style.display = 'none';
-    document.getElementById('imgCard').style.display = 'none';
-    document.getElementById('cvv').style.display = 'flex';
-    document.getElementById('cvv').style.transform = 'scaleX(-1)';
-    document.getElementById('card-company').style.transform = 'scaleX(-1)';
-    // }
 	  $('#cardCVVInput').on('input', function() {
+  	rotate180();
 	    var number = document.getElementById('cardCVVInput').value;
 	    if (number.match(/[а-я,А-я,a-z,A-z]/g)){
 	      alert("Введіть будь ласка тільки цифри");
@@ -87,12 +91,6 @@ window.onload = function(){
 	    document.getElementById('cvvCode').innerHTML = number;
 	  }})
   });
-  // $('#cardNumberInput1').blur(function() {
-  //   var number = document.getElementById('cardNumberInput1');
-  //   if (number.value.length === 0){
-  //     $('#cardNumber1').html('**** **** **** ****');
-  //   }
-  // })
   $('#cardOwnerInput').blur(function() {
     var owner = document.getElementById('cardOwnerInput');
     if (owner.value.length < 4){
@@ -103,15 +101,22 @@ window.onload = function(){
   $('#cardCVVInput').blur(function() {
     var owner = document.getElementById('cardCVVInput');
     var rotate = document.getElementById('Rotate');
-
-    rotate.style.transform = "rotateY(360deg)";
-    rotate.style.transition  = "all 1.5s";
-    document.getElementById('card-company').style.transform = 'scaleX(1)';
-    document.getElementById('card-number').style.display = 'flex';
-    document.getElementById('card-expire').style.display = 'flex';
-    document.getElementById('card-holder').style.display = 'flex';
-    document.getElementById('cvv').style.display = 'none';
-    document.getElementById('imgCard').style.display = 'inline';
+  	rotate90();
+  	setTimeout(rotate0,750);
+  	function rotate90(){
+	  	rotate.style.transform = "rotateY(90deg)";
+	  	rotate.style.transition  = "all 0.75s";
+    }
+    function rotate0(){
+	    document.getElementById('card-company').style.transform = 'scaleX(1)';
+	    document.getElementById('card-number').style.display = 'flex';
+	    document.getElementById('card-expire').style.display = 'flex';
+	    document.getElementById('card-holder').style.display = 'flex';
+	    document.getElementById('cvv').style.display = 'none';
+	    document.getElementById('imgCard').style.display = 'inline';
+	   	rotate.style.transform = "rotateY(0deg)";
+	    rotate.style.transition  = "all 0.75s";
+  	}
     if (owner.value.length < 3){
       $('#cardCVVInput').val('');
       $('#cvvCode').html('***');
